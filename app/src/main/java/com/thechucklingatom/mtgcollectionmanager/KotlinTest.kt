8d3fd3539db10337639c371d89dbf6344a05fa8e
Robert Putnam <thechucklingatom@gmail.com>
2016-12-13 09:46:25 -0700

    Kotlin and anko with a simple listview, now working on moving in all the logic that was in the last non kotlin  version.

--- app/src/main/java/com/thechucklingatom/mtgcollectionmanager/KotlinTest.kt
+++ app/src/main/java/com/thechucklingatom/mtgcollectionmanager/KotlinTest.kt
@@ -1,24 +1,45 @@
 package com.thechucklingatom.mtgcollectionmanager
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import org.jetbrains.anko.*
+import android.widget.ListView
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.listView
+import org.jetbrains.anko.verticalLayout
 
-class KotlinTest : AppCompatActivity() {
+class KotlinTest : Activity() {
+
+    val arr = Array(5, Int::toString)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        verticalLayout{
-            val name = editText()
-            button("Say Hello"){
-                onClick { toast("Hello, ${name.text}")}
+
+        verticalLayout {
+
+            toolbar {
+                backgroundResource = R.color.colorPrimary
+                setTitleTextColor(Color.WHITE)
+                title = "test bar"
+            }
+
+            id = R.layout.test_text_layout
+
+            listView {
+                id = R.id.test_text_view
             }
         }
+
+        val cardAdapter: CardAdapter? = CardAdapter(this)
+
+        val tempView: ListView = findViewById(R.id.test_text_view) as ListView
+
+        tempView.adapter = cardAdapter
+
+        cardAdapter?.notifyDataSetChanged()
+
     }
 
 }
