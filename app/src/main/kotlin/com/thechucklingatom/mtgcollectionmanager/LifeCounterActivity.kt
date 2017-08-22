74a738ecef1ad8c030fce99689217d7d4d5e5f20
Robert Putnam <thechucklingatom@gmail.com>
2017-08-21 21:32:45 -0600

    Tiny bit of lifecounter work

--- app/src/main/kotlin/com/thechucklingatom/mtgcollectionmanager/LifeCounterActivity.kt
+++ app/src/main/kotlin/com/thechucklingatom/mtgcollectionmanager/LifeCounterActivity.kt
@@ -1,14 +1,45 @@
 package com.thechucklingatom.mtgcollectionmanager
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import com.thechucklingatom.mtgcollectionmanager.Presenters.LifeCounterPresenter
-import com.thechucklingatom.mtgcollectionmanager.Views.LifeCounterView
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import org.jetbrains.anko.*
 
 class LifeCounterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_counter)
+
+        setLayout()
+    }
+
+    fun setLayout(){
+        verticalLayout {
+            val upperTotal = textView{
+                text = "19"
+                textSize = sp(50f).toFloat()
+                gravity = Gravity.CENTER
+                rotation = 180f
+                typeface = Typeface.DEFAULT_BOLD
+                textColor = Color.BLACK
+            }
+
+            var lowerTotal = textView {
+                text = "20"
+                textSize = sp(50f).toFloat()
+                gravity = Gravity.CENTER
+                typeface = Typeface.DEFAULT_BOLD
+                textColor = Color.BLACK
+            }
+
+            button {
+                onClick {
+                    lowerTotal.text = "35"
+                }
+            }
+        }
     }
 }
